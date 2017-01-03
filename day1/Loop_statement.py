4b96a5b97aa712b1f6528d447184a653d24ff6a4
songbing <songbing5132163.com>
2017-01-03 10:59:50 +0800

    增加while实例

--- day1/Loop_statement.py
+++ day1/Loop_statement.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-for i in  list(range(1,10)):
-    print("\r")
-    print(i,end=" ")
-    for a in 'spam':
-        print(a,end=" ")
+# for i in  list(range(1,10)):
+#     print("\r")
+#     print(i,end=" ")
+#     for a in 'spam':
+#         print(a,end=" ")
 
 
 # for i in list(range(1,10)):
@@ -14,3 +14,25 @@ for i in  list(range(1,10)):
 #         if j==i:
 #             print("\r")
 
+
+
+
+number=10
+
+Flag=True
+while Flag:
+    get_number = input("Please input your lucky number: ")
+    if get_number.isdigit():
+        get_number=int(get_number)
+    else:
+        print("need number!")
+        continue
+    if get_number==number:
+        print("This is your lucky number!")
+        Flag=False
+    elif get_number>number:
+        print("The number you entered is too large")
+    elif get_number<number:
+        print("The number you entered is too small.")
+else:
+    print("game over!")

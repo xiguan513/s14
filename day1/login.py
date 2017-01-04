bb2e10169cdd49f76744fe66ac223ecf99899642
songbing <songbing5132163.com>
2017-01-04 13:50:24 +0800

    增加用户登录接口，清除黑名单用户功能

--- day1/login.py
+++ day1/login.py
@@ -2,6 +2,12 @@
 # -*- coding: UTF-8 -*-
 
 with open("passwd") as lock_file:
-    for line in lock_file.readlines():
-        if "admin" in line.strip():
-            print("111111")
+    lock_user=lock_file.readlines()
+
+if [ user for user in lock_user if "song" in user.strip()  ]:
+    print("111111")
+
+import fileinput
+
+for user in fileinput.input("passwd",backup=".bak",inplace=1):
+    print(user.rstrip().replace('test',''))
--- day1/login_api.py
+++ day1/login_api.py
@@ -6,35 +6,43 @@ Flag_user=True
 user_pass={"song":123456,
            "admin":123456,
            "bing":123456}
-import sys
+import fileinput
+
+file_lock="passwd"
 
 while Flag_user:
     pass_num = 0 #设置标志位
     login_name=input("Please input your name: ").strip()
+
     if login_name in user_pass:#判断用户是否存在
-        with open("passwd") as lock_file:
-            for line in lock_file.readlines():
-                if login_name  in line.strip():#判断是否在黑名单
-                    print("This user has been locked")
-                    sys.exit()
-        while 3>pass_num:#判断用户密码输入次数
-            login_pass=int(input("Please input your password: "))
-            if login_pass == user_pass[login_name]:#判断密码是否正确
-                print("Welcome")
-                break
-            else:
-                pass_num+=1
-                print("password is  error")
-        else:
-            print("Wrong password three times, lock user")
-            with open("passwd", "a+") as lock_user:#添加黑名单
-                lock_user.write("\r")
-                lock_user.write(login_name)
+        with open(file_lock) as lock_file:
+            lock_list=lock_file.readlines()
+        if [ user for user in lock_list if login_name in user.strip()  ]:#通过列表表达式判断是否在黑名单
+            print("This user has been locked")
             continue
-        break
     else:
         print("Not Found User!")
         continue
+
+    while 3 > pass_num:  # 判断用户密码输入次数
+        login_pass = int(input("Please input your password: "))
+        if login_pass == user_pass[login_name]:  # 判断密码是否正确
+            print("Welcome")
+            if login_name == "admin":
+                unlock_user = input("Please inpur your unlock user: ")
+                for user in fileinput.input(file_lock, backup=".bak", inplace=1):#清除黑名单用户
+                    print(user.rstrip().replace(unlock_user, ''))#fileinput 替换方法
+            break
+        else:
+            pass_num += 1
+            print("password is  error")
+    else:
+        print("Wrong password three times, lock user")
+        with open("passwd", "a+") as lock_user:  # 添加黑名单
+            lock_user.write("\r")
+            lock_user.write(login_name)
+        continue
+    break
 
 
 

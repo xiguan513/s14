bfbae489891c3f62d09fa96f47f65fdf55eb1cd4
songbing <songbing5132163.com>
2017-01-11 10:29:05 +0800

    商店包导入

--- day2/login.py
+++ day2/login.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+
+name_uesr={"song":123456,
+           "bing":123456,
+           "admin":123456}
+
+
+while True:
+    login_name=input("Please input your name: ").strip()
+    if login_name in name_uesr:
+        while True:
+            try:
+                login_name_pass=int(input("Please input your this user passwd: "))
+                if login_name_pass==name_uesr[login_name]:
+                    print("Welcome to shop!")
+            except ValueError:
+                print("password is error")
+    else:
+        print("Sorry not to find this user!")

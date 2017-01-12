7834c98664a5bd7a1ed3dbd2aa40462f0f3a6686
songbing <songbing5132163.com>
2017-01-12 11:46:32 +0800

    增加二分查找算法函数bisect

--- day2/Two_point_search.py
+++ day2/Two_point_search.py
@@ -19,5 +19,25 @@ def two(list,num):
         else:
             print(list[mid]) #如果等于就直接输出列表中的数字
 
-if __name__=="__main__":
-    two(num_list,num)
+if __name__ == "__main__":
+    two(num_list, num)
+
+#二分查找函数
+import bisect
+
+L = [1, 3, 3, 6, 8, 12, 15]
+
+
+x_insert_point = bisect.bisect_left(L, num)  # 在L中查找x，x存在时返回x左侧的位置，x不存在返回应该插入的位置..这是3存在于列表中，返回左侧位置１
+print(x_insert_point)
+
+x_insert_point = bisect.bisect_right(L, num)  # 在L中查找x，x存在时返回x右侧的位置，x不存在返回应该插入的位置..这是3存在于列表中，返回右侧位置３
+print(x_insert_point)
+
+
+x_insort_left = bisect.insort_left(L, num)  # 将x插入到列表L中，x存在时插入在左侧
+print(L)
+
+x_insort_rigth = bisect.insort_right(L, num)  # 将x插入到列表L中，x存在时插入在右侧　　　　
+print(L)
+

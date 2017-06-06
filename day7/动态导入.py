f9d3266b5fdc94cbd5f70fdd010da91787564696
songbing <songbing5132163.com>
2017-06-06 14:46:52 +0800

    模块动态导入两种方法

--- day7/动态导入.py
+++ day7/动态导入.py
@@ -4,6 +4,13 @@
 #导入lib包下面的aa模块
 
 mod=__import__("lib.aa")
-
 ojb=mod.aa.C()
 print ojb.name
+
+
+
+#官方建议使用方法
+import importlib
+mod1=importlib.import_module('lib.aa')
+obj=mod1.C()
+print obj.name
